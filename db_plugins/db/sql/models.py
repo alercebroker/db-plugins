408c3cdbe255b99ab65c8003db3f57b6beda14a5
Camilo Valenzuela <cacke91@gmail.com>
2020-10-19 10:15:20 -0300

    Removed fid from non_detection PK, added catid to xmatch PK

--- db_plugins/db/sql/models.py
+++ db_plugins/db/sql/models.py
@@ -140,7 +140,7 @@ class Xmatch(Base):
     __tablename__ = "xmatch"
 
     oid = Column(String, ForeignKey("object.oid"), primary_key=True)
-    catid = Column(String, nullable=False)
+    catid = Column(String, nullable=False, primary_key=True)
     oid_catalog = Column(String, nullable=False)
     dist = Column(Float(precision=53), nullable=False)
     class_catalog = Column(String)
@@ -220,7 +220,7 @@ class NonDetection(Base, generic.AbstractNonDetection, Commons):
     __tablename__ = "non_detection"
 
     oid = Column(String, ForeignKey("object.oid"), primary_key=True)
-    fid = Column(Integer, primary_key=True)
+    fid = Column(Integer)
     mjd = Column(Float(precision=53), primary_key=True)
     diffmaglim = Column(Float)
     __table_args__ = (
@@ -410,5 +410,3 @@ class Pipeline(Base, generic.AbstractPipeline):
     step_id_out = Column(String)
     step_id_stamp = Column(String)
     date = Column(DateTime, nullable=False)
-
-
