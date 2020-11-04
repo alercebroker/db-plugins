b77192d248e736ed332cca75a281ef5a49dcb858
Camilo Valenzuela <cacke91@gmail.com>
2020-11-04 14:38:57 -0300

    Added created_on and last_updated to outlier score model

--- db_plugins/db/sql/models.py
+++ db_plugins/db/sql/models.py
@@ -114,6 +114,8 @@ class Outlier(Base):
     class_name = Column(String, primary_key=True)
     outlier_version = Column(String, primary_key=True)
     score = Column(Float, nullable=False)
+    created_on = Column(DateTime, default=datetime.datetime.now)
+    last_updated = Column(DateTime, onupdate=datetime.datetime.now)
 
     __table_args__ = (
         Index("ix_outlier_oid", "oid", postgresql_using="hash"),
