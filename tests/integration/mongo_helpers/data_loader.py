8ed4da33eff8effd7d795be2bcf11dd8aa13dcdd
Pedro <pegallardo@inf.udec.cl>
2022-12-13 10:54:16 -0300

    feat: probability generator added

--- tests/integration/mongo_helpers/data_loader.py
+++ tests/integration/mongo_helpers/data_loader.py
@@ -4,6 +4,7 @@ import random
 
 random.seed(42)
 n_docs = int(1e5)
+n_class = 10
 
 
 def object_factory(index):
@@ -21,32 +22,40 @@ def object_factory(index):
         meanra=100.0,
         meandec=50.0,
         ndet=2,
-        probabilities=[{
-            "classifier_name": "stamp_classifier",
-            "classifier_version": "stamp_classifier_1.0.0",
-            "class_name": "CLASS1",
-            "probability": stamp_prob,
-            "ranking": 1 if stamp_prob > 0.5 else 2,
-        }, {
-            "classifier_name": "stamp_classifier",
-            "classifier_version": "stamp_classifier_1.0.0",
-            "class_name": "CLASS2",
-            "probability": 1 - stamp_prob,
-            "ranking": 1 if stamp_prob <= 0.5 else 2,
-        }, {
-            "classifier_name": "lc_classifier",
-            "classifier_version": "lc_classifier_1.0.0",
-            "class_name": "CLASS1",
-            "probability": lc_prob,
-            "ranking": 1 if lc_prob > 0.5 else 2,
-        }, {
-            "classifier_name": "lc_classifier",
-            "classifier_version": "lc_classifier_1.0.0",
-            "class_name": "CLASS2",
-            "probability": 1 - lc_prob,
-            "ranking": 1 if lc_prob <= 0.5 else 2,
-        }])
+        probabilities=generate_probabilities()
+    )
 
+def generate_probabilities():
+    classifiers = [
+        "stamp_classifier",
+        "lc_classifier"
+    ]
+    classes = [f"CLASS{i + 1}" for i in range(n_class)]
+
+    generated_probabilities = []
+
+    for classifier in classifiers:
+        random.shuffle(classes)
+        class_probs = [random.random() for _ in classes]
+        class_probs = sorted([prob / sum(class_probs) for prob in class_probs], reverse=True)
+
+        generated_probabilities.extend(
+            [
+                probability_factory(classifier, class_name, class_probs[rank], rank + 1)
+                for rank, class_name in enumerate(classes)
+            ]
+        )
+
+    return generated_probabilities
+
+def probability_factory(classifier_name, class_name, prob, rank):
+    return {
+        "classifier_name": classifier_name,
+        "classifier_version": f"{classifier_name}_1.0.0",
+        "class_name": class_name,
+        "probability": prob,
+        "ranking": rank,
+    }
 
 def generate_data():
     return [object_factory(i) for i in range(n_docs)]
