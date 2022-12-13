17dd4986dbe8795b84db0ed840c03ccd42f09655
Pablo Castellanos <pablocastellanos@gmail.com>
2022-12-13 12:12:24 -0300

    tests: added dbstats print out

--- tests/integration/mongo_helpers/operations.py
+++ tests/integration/mongo_helpers/operations.py
@@ -1,17 +1,25 @@
-from pprint import pprint
 from db_plugins.db.mongo.connection import MongoConnection
 
 
-def _get_execution_stats(query_info, command=False):
+def _get_execution_stats(query_info, out=False):
     execution_stats = query_info["executionStats"]
     total_ms = execution_stats["executionTimeMillis"]
     docs_examined = execution_stats["totalDocsExamined"]
+    docs_returned = execution_stats["nReturned"]
     keys_examined = execution_stats["totalKeysExamined"]
 
-    if command:
-        pprint(execution_stats)
+    if out:
+        print(f'\nResult\n  Time: {total_ms} ms\n  Docs returned {docs_returned}\n  Docs examined: {docs_examined}\n  Keys examined: {keys_examined}')
 
     return total_ms, docs_examined, keys_examined
+
+
+def _get_dbstats(dbstats_info):
+    index_size = dbstats_info["indexSize"]
+    total_size = dbstats_info["totalSize"]
+
+    conv = 1024 ** 2
+    print(f'\nDB\n  Total size: {total_size / conv:.1f} MB\n  Index size {index_size / conv: .1f} MB\n  Index proportion: {index_size / total_size:.3f}')
 
 
 def find_objects_batch_explained(connection: MongoConnection, aid_list: list):
@@ -40,5 +48,8 @@ def find_objects_by_probability(connection: MongoConnection, classifier_name, cl
                 "ranking": 1
             }
             }}).explain()
+
+    dbstats = connection.database.command("dbstats")
+    _get_dbstats(dbstats)
 
     return _get_execution_stats(query_info, True)
